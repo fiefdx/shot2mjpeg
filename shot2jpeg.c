f0fdb9e833ad5c609ca9a3f3f0ca3fe7981bd4b4
fiefdx <fiefdx@163.com>
2018-06-11 13:31:14 +0800

    first commit

---
+++ shot2jpeg.c
@@ -0,0 +1,143 @@
+// screenshot to jpeg for linux
+
+#include "shot2jpeg.h"
+
+#define IMG_AT(x, y, i) image->data[(((y_width) + (x)) << 2) + (i)]
+#define RGBA_AT(x, y, i) rgba[(((y_width) + (x)) << 2) + (i)]
+#define RGB_AT(x, y, i) rgb[((y_width) + (x)) * 3 + (i)]
+
+xcb_image_t *take_screenshot(xcb_connection_t *conn, xcb_screen_t *screen) {
+    return xcb_image_get(conn,
+        screen->root,
+        0, 0,
+        screen->width_in_pixels, screen->height_in_pixels,
+        UINT32_MAX,
+        XCB_IMAGE_FORMAT_Z_PIXMAP);
+}
+
+xcb_pixmap_t image_to_pixmap(xcb_connection_t *conn, xcb_screen_t *screen, xcb_image_t *image) {
+    xcb_pixmap_t pixmap = xcb_generate_id(conn);
+    xcb_create_pixmap(conn, 24, pixmap, screen->root, image->width, image->height);
+
+    xcb_gcontext_t gc = xcb_generate_id(conn);
+    xcb_create_gc(conn, gc, pixmap,
+        XCB_GC_FOREGROUND | XCB_GC_BACKGROUND,
+        (uint32_t[]){ screen->black_pixel, 0xffffff });
+
+    xcb_image_put(conn, pixmap, gc, image, 0, 0, 0);
+
+    return pixmap;
+}
+
+void get_rgba_image_data(xcb_image_t *image, uint8_t *rgba) {
+    for (int y = 0; y < image->height; y++) {
+        int y_width = y*image->width;
+        for (int x = 0; x < image->width; x++) {
+            RGBA_AT(x, y, 0) = IMG_AT(x, y, 2); // r
+            RGBA_AT(x, y, 1) = IMG_AT(x, y, 1); // g
+            RGBA_AT(x, y, 2) = IMG_AT(x, y, 0); // b
+            RGBA_AT(x, y, 3) = IMG_AT(x, y, 3); // a
+        }
+    }
+}
+
+void get_rgba_image_data2(xcb_image_t *image, uint8_t *rgba) {
+    memcpy(rgba, image->data, image->size);
+    for (int y = 0; y < image->height; y++) {
+        int y_width = y*image->width;
+        for (int x = 0; x < image->width; x++) {
+            RGBA_AT(x, y, 0) = IMG_AT(x, y, 2); // r
+            RGBA_AT(x, y, 2) = IMG_AT(x, y, 0); // b
+        }
+    }
+}
+
+void get_rgb_image_data(xcb_image_t *image, uint8_t *rgb) {
+    for (int y = 0; y < image->height; y++) {
+        int y_width = y*image->width;
+        for (int x = 0; x < image->width; x++) {
+            RGB_AT(x, y, 0) = IMG_AT(x, y, 2); // r
+            RGB_AT(x, y, 1) = IMG_AT(x, y, 1); // g
+            RGB_AT(x, y, 2) = IMG_AT(x, y, 0); // b
+        }
+    }
+}
+
+void write_to_jpeg(char *filename, int quality, xcb_image_t *image) {
+    uint8_t data[image->width*image->height*4];
+    get_rgba_image_data2(image, data);
+    struct jpeg_compress_struct cinfo;
+    struct jpeg_error_mgr jerr;
+    FILE *outfile;
+    JSAMPROW row_pointer[1];
+    int row_stride;
+    cinfo.err = jpeg_std_error(&jerr);
+    jpeg_create_compress(&cinfo);
+    if ((outfile = fopen(filename, "wb")) == NULL) {
+        fprintf(stderr, "can't open %s\n", filename);
+        exit(1);
+    }
+    jpeg_stdio_dest(&cinfo, outfile);
+
+    cinfo.image_width = image->width;
+    cinfo.image_height = image->height;
+    cinfo.input_components = 4;
+    cinfo.in_color_space = getJCS_EXT_RGBA();
+    if (cinfo.in_color_space == JCS_UNKNOWN) {
+        fprintf(stderr, "JCS_EXT_RGBA is not supported (probably built without libjpeg-trubo)");
+        exit(1);
+    }
+
+    jpeg_set_defaults(&cinfo);
+    jpeg_set_quality(&cinfo, quality, TRUE);
+
+    jpeg_start_compress(&cinfo, TRUE);
+
+    row_stride = image->width * 4;
+
+    while (cinfo.next_scanline < cinfo.image_height) {
+        row_pointer[0] = &data[cinfo.next_scanline * row_stride];
+        (void) jpeg_write_scanlines(&cinfo, row_pointer, 1);
+    }
+
+    jpeg_finish_compress(&cinfo);
+    fclose(outfile);
+    jpeg_destroy_compress(&cinfo);
+}
+
+void write_to_jpeg_buffer(FILE *stream, int quality, xcb_image_t *image) {
+    uint8_t data[image->width*image->height*4];
+    get_rgba_image_data2(image, data);
+    struct jpeg_compress_struct cinfo;
+    struct jpeg_error_mgr jerr;
+    JSAMPROW row_pointer[1];
+    int row_stride;
+    cinfo.err = jpeg_std_error(&jerr);
+    jpeg_create_compress(&cinfo);
+    jpeg_stdio_dest(&cinfo, stream);
+
+    cinfo.image_width = image->width;
+    cinfo.image_height = image->height;
+    cinfo.input_components = 4;
+    cinfo.in_color_space = getJCS_EXT_RGBA();
+    if (cinfo.in_color_space == JCS_UNKNOWN) {
+        fprintf(stderr, "JCS_EXT_RGBA is not supported (probably built without libjpeg-trubo)");
+        exit(1);
+    }
+
+    jpeg_set_defaults(&cinfo);
+    jpeg_set_quality(&cinfo, quality, TRUE);
+
+    jpeg_start_compress(&cinfo, TRUE);
+
+    row_stride = image->width * 4;
+
+    while (cinfo.next_scanline < cinfo.image_height) {
+        row_pointer[0] = &data[cinfo.next_scanline * row_stride];
+        (void) jpeg_write_scanlines(&cinfo, row_pointer, 1);
+    }
+
+    jpeg_finish_compress(&cinfo);
+    fclose(stream);
+    jpeg_destroy_compress(&cinfo);
+}
---
+++ shot2mjpeg_server.c
@@ -0,0 +1,206 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <sys/time.h>
+#include <string.h>
+#include "httpd.h"
+#include "httpd_priv.h"
+#include "shot2jpeg.h"
+
+
+struct FileContent {
+    unsigned long length;
+    char *buffer;
+};
+
+struct FileContent read_file(char *name) {
+    FILE *file;
+    char *buffer;
+    unsigned long fileLen;
+    struct FileContent result;
+
+    //Open file
+    file = fopen(name, "rb");
+    if (!file) {
+        fprintf(stderr, "Unable to open file %s", name);
+        return;
+    }
+    
+    //Get file length
+    fseek(file, 0, SEEK_END);
+    fileLen=(ftell(file));
+    printf("fileLen: %d\n", fileLen);
+    fseek(file, 0, SEEK_SET);
+
+    //Allocate memory
+    buffer=(char *)malloc((fileLen)+1);
+    if (!buffer) {
+        fprintf(stderr, "Memory error!");
+        fclose(file);
+        return;
+    }
+
+    //Read file contents into buffer
+    fread(buffer, fileLen, 1, file);
+    fclose(file);
+
+    result.length = fileLen;
+    result.buffer = buffer;
+
+    //Do what ever with buffer
+
+    // free(buffer);
+    return result;
+}
+
+int nsleep(long miliseconds) {
+    struct timespec req, rem;
+
+    if(miliseconds > 999)
+    {   
+        req.tv_sec = (int)(miliseconds / 1000);                            /* Must be Non-Negative */
+        req.tv_nsec = (miliseconds - ((long)req.tv_sec * 1000)) * 1000000; /* Must be in range of 0 to 999999999 */
+    }   
+    else
+    {   
+        req.tv_sec = 0;                         /* Must be Non-Negative */
+        req.tv_nsec = miliseconds * 1000000;    /* Must be in range of 0 to 999999999 */
+    }   
+
+    return nanosleep(&req , &rem);
+}
+
+void jpeg_handler(httpd *server, httpReq *request) {
+    httpdSetContentType(server, request, "image/jpeg");
+    httpdSendHeaders(server, request);
+    struct FileContent buffer;
+    buffer = read_file("../example.jpeg");
+    printf("buffer length: %d\n", buffer.length);
+    request->response.responseLength += buffer.length;
+    _httpd_net_write(request->clientSock, buffer.buffer, buffer.length);
+    // free(&buffer); // *** Error in `./http_example_server': double free or corruption (out): 0x00007ffd72cd9580 ***
+    return;
+}
+
+void mjpeg_handler(httpd *server, httpReq *request) {
+    printf(">>>>>> mjpeg_handler start\n");
+    httpdSetContentType(server, request, "multipart/x-mixed-replace;boundary=\"mjpegstream\"");
+    httpdAddHeader(server, request, "Cache-Control: no-store, no-cache, must-revalidate, pre-check=0, post-check=0, max-age=0");
+    // httpdAddHeader(server, request, "Content-Type: multipart/x-mixed-replace;boundary=--boundary");
+    httpdAddHeader(server, request, "Pragma: no-cache");
+    // httpdAddHeader(server, request, "Connection: close");
+    httpdSendHeaders(server, request);
+
+    int quality = 75;
+    printf("Quality: %d\n", quality);
+
+    struct timeval s, ss;
+    struct FileContent buffer;
+
+    xcb_connection_t *conn = xcb_connect(NULL, NULL);
+
+    const xcb_setup_t *setup = xcb_get_setup(conn);
+    xcb_screen_iterator_t iter = xcb_setup_roots_iterator(setup);
+    xcb_screen_t *screen = iter.data;
+
+    for (int i = 0; 1; i++) {
+        gettimeofday(&s, NULL);
+        printf("frame: %04d\n", i);
+        xcb_image_t *screenshot;
+        screenshot = take_screenshot(conn, screen);
+        xcb_pixmap_t pixmap = image_to_pixmap(conn, screen, screenshot);
+        printf("screenshot: width: %d, height: %d, size: %d\n", screenshot->width, screenshot->height, screenshot->size);
+        // printf("pixmap: %d\n", pixmap);
+
+        char *bp;
+        size_t size;
+        FILE *stream;
+        stream = open_memstream(&bp, &size);
+        write_to_jpeg_buffer(stream, quality, screenshot);
+        buffer.buffer = bp;
+        buffer.length = size;
+
+        char *header = (char *)malloc((150)+1);
+        sprintf(header, "--mjpegstream\r\nContent-Type: image/jpeg\r\nContent-Length: %d\r\n\r\n", buffer.length);
+        int r;
+        r = _httpd_net_write(request->clientSock, header, strlen(header));
+        printf("socket r: %d\n", r);
+        if (r == 0) {
+            r = _httpd_net_write(request->clientSock, buffer.buffer, buffer.length);
+        } else {
+            break;
+        }
+
+        xcb_image_destroy(screenshot);
+        xcb_free_pixmap(conn, pixmap);
+        free(bp);
+        free(header);
+        // // printf("buffer: size: %d\n", size);
+        gettimeofday(&ss, NULL);
+        long interval = 40 - (((ss.tv_sec - s.tv_sec) * 1000000 + (ss.tv_usec - s.tv_usec))/1000); // ms
+        if (interval > 0) {
+            nsleep(interval);
+            printf("sleep: %dms\n", interval);
+        }
+    }
+    free(buffer.buffer);
+    printf(">>>>>> mjpeg_handler end\n");
+}
+
+void sig_ign(int s) {
+    printf("Caught SIGPIPE\n");
+}
+
+int main(argc, argv)
+    int argc;
+    char *argv[];
+{
+    httpd   *server;
+    httpReq *request;
+    struct timeval timeout;
+    int result;
+
+    signal(SIGPIPE, SIG_IGN);
+    /*
+    ** Create a server and setup our logging
+    */
+    server = httpdCreate(NULL, 8080);
+    if (server == NULL)
+    {
+        perror("Can't create server");
+        exit(1);
+    }
+    httpdSetAccessLog(server, stdout);
+    httpdSetErrorLog(server, stderr);
+
+    /*
+    ** Setup some content for the server
+    */
+    httpdAddCContent(server, "/", "mjpeg", HTTP_TRUE, NULL, mjpeg_handler);
+    httpdAddCContent(server, "/", "jpeg", HTTP_TRUE, NULL, jpeg_handler);
+
+    /*
+    ** Go into our service loop
+    */
+    printf("mjpeg server start\n");
+    while (1)
+    {
+        timeout.tv_sec = 5;
+        timeout.tv_usec = 0;
+        request = httpdReadRequest(server, &timeout, &result);
+        if (request == NULL && result == 0)
+        {
+            /* Timed out. Go do something else if needed */
+            continue;
+        }
+
+        if (result < 0)
+        {
+            /* Error occurred */
+            continue;
+        }
+        httpdProcessRequest(server, request);
+        httpdEndRequest(server, request);
+    }
+    printf("mjpeg server end\n");
+}
